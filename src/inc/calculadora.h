c5af613b055cea6683a047b9642112247c7de3da
Gerez Jimenez Juan Jose Armando <jjagerezjimenez@gmail.com>
2023-05-28 18:53:01 -0300

    modificacion archivo H

---
+++ src/inc/calculadora.h
@@ -0,0 +1,13 @@
+#include <stdint.h>
+#include <stdbool.h>
+
+
+typedef struct calculadora_s * calculadora_t:
+
+typedef int(*funcion_t)(int, int):
+
+calculadora_t CrearCalculadora(void):
+
+bool AgregarOperacion(calculadora_t calculadora, char operador, funcion_t funcion):
+
+int Calcular(calculadora_t calculadora, char * cadena):
