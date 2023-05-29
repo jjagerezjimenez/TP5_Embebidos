ba2637438d558d3d79e216a6b7b275fa841061b4
Juan Jose Armando Gerez Jimenez <jjagerezjimenez@gmail.com>
2023-05-29 15:24:35 -0300

    documentacion main.c

--- src/main.c
+++ src/main.c
@@ -1,22 +1,60 @@
+
+/**
+ * @file main.c
+ *
+ * @brief Ejemplo de uso de la calculadora.
+ */
 #include "calculadora.h"
 #include "stdio.h"
 
+/**
+ * @brief Función que realiza la suma de dos números enteros.
+ *
+ * @param a El primer número entero.
+ * @param b El segundo número entero.
+ * @return El resultado de la suma.
+ */
 int suma(int a, int b){
     return a + b;
 }
 
+/**
+ * @brief Función que realiza la resta de dos números enteros.
+ *
+ * @param a El primer número entero.
+ * @param b El segundo número entero.
+ * @return El resultado de la resta.
+ */
 int resta(int a, int b){
     return a - b;
 }
 
+
+/**
+ * @brief Función que realiza la multiplicación de dos números enteros.
+ *
+ * @param a El primer número entero.
+ * @param b El segundo número entero.
+ * @return El resultado de la multiplicación.
+ */
 int producto(int a, int b){
     return a * b;
 }
 
+/**
+ * @brief Función que realiza la división de dos números enteros.
+ *
+ * @param a El primer número entero.
+ * @param b El segundo número entero.
+ * @return El resultado de la división.
+ */
 int division(int a, int b){
     return a / b;
 }
 
+/**
+ * @brief Función principal del programa.
+ */
 int main(void){
     int resultado;
 
